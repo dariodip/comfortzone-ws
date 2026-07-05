7c04a33efc98507888b866e4fd9d1cae3b5d1e53
Dario Di Pasquale <darionovantadue@gmail.com>
2026-07-05 10:25:32 +0200

    feat: add Docker and Wrangler configuration, integrate Cloudflare adapter, and set up GitHub Actions for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,18 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://comfortzone.it",
+
   vite: {
     plugins: [tailwindcss()],
     server: {
       allowedHosts: ["overspend-curtain-cash.ngrok-free.dev"]
     }
-  }
+  },
+
+  adapter: cloudflare()
 });
